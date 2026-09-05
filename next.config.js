2fd609c21d7f90eb797b12a4ed241e6007f80e13
PRAVEEN KUMAR KODAMANCHILI <269670660+praveenrai-hue@users.noreply.github.com>
2026-09-05 16:20:53 +0530

    Initialize project structure with configuration files and dependencies

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.runtimeChunk = 'single';
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
